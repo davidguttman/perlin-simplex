def4ec187aaafbdd6a379992d9246dcf74e0c3a3
David Guttman <david@davidguttman.com>
2013-12-01 15:20:16 -0800

    added 3d tests

--- test/index.js
+++ test/index.js
@@ -15,3 +15,7 @@ var simplex = new Simplex(TestRandom)
 assert.equal( simplex.noise(1,2),     -0.4705299224716831)
 assert.equal( simplex.noise(1,2.001), -0.474245420644826)
 assert.equal( simplex.noise(1,3),     0.4989247905504145)
+
+assert.equal( simplex.noise3d(1,2,3), 0)
+assert.equal( simplex.noise3d(1,2,4), 0.05393909465020551)
+assert.equal( simplex.noise3d(1,2,5), 0.3800497942386826)
